7fdee143a585ba86015df7d4660324f4bc0cf0f4
utkarshg12anmak <utkarsh.gupta@anmaksolar.com>
2025-06-26 00:38:06 +0530

    removed landing page ,changed expenses view logic, changed items pagination

--- expenses/views.py
+++ expenses/views.py
@@ -1,8 +1,16 @@
+#expenses/views.py
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from .models import Expense, ExpenseRole
 from django.core.exceptions import PermissionDenied
 from .forms import ExpenseForm
+
+from django.db.models import Q
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Expense
+from profiles.models import DepartmentMembership
 
 class ExpenseListView(ListView):
     model = Expense
@@ -11,13 +19,39 @@ class ExpenseListView(ListView):
     ordering = ['-updated_at'] 
 
     def get_queryset(self):
-        user = self.request.user
-        role = getattr(user, "expense_role", None).role
+        me = self.request.user
 
-        if role in (ExpenseRole.Role.GLOBAL_VIEWER, ExpenseRole.Role.GLOBAL_EDITOR):
-            return Expense.objects.all().order_by("-created_at")
-        # SELF_SERVICE
-        return Expense.objects.filter(created_by=user).order_by("-created_at")
+        # Start with your own ID
+        allowed_user_ids = {me.id}
+
+        # Gather subordinates from *all* departments the user belongs to
+        for mem in DepartmentMembership.objects.filter(user=me):
+            dept = mem.department_id
+
+            if mem.level == 1:
+                # add all L2 & L3 in this department
+                subs = DepartmentMembership.objects.filter(
+                    department_id=dept,
+                    level__in=[2, 3]
+                ).values_list("user_id", flat=True)
+
+            elif mem.level == 2:
+                # add all L3 in this department
+                subs = DepartmentMembership.objects.filter(
+                    department_id=dept,
+                    level=3
+                ).values_list("user_id", flat=True)
+
+            else:
+                # Level 3: no subordinates
+                subs = []
+
+            allowed_user_ids.update(subs)
+
+        # Now build one filter on created_by__in
+        return Expense.objects.filter(
+            created_by_id__in=allowed_user_ids
+        ).order_by("-created_at")
 
 class ExpenseCreateView(CreateView):
     model = Expense
